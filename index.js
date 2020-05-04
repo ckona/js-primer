e9141bd8ab105f127845276e8495a66a505cbad1
ckona <pagudonguri@gmail.com>
2020-05-04 15:07:19 +0900

    Add fetchUserInfo function

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("index.js: loaded");
+
+const fetchUserInfo = (userInfo) => {
+  const userId = "ckona";
+
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+      console.log(response.status);
+
+      if (!response.ok) {
+        console.error("error response: ", response);
+        return nil;
+      } else {
+        return response.json().then(userInfo => {
+          console.log(userInfo);
+        });
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
