a38e47a19a2b3272e74a590a28df33800e669c8e
ckona <pagudonguri@gmail.com>
2020-05-04 15:16:44 +0900

    Display github user info

--- index.js
+++ index.js
@@ -12,11 +12,42 @@ const fetchUserInfo = (userInfo) => {
         return nil;
       } else {
         return response.json().then(userInfo => {
-          console.log(userInfo);
+          const view = escapeHTML`
+            <h4>${userInfo.name} (@${userInfo.login})</h4>
+            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+            <dl>
+              <dt>Location</dt>
+              <dd>${userInfo.location}</dd>
+              <dt>Repositories</dt>
+              <dd>${userInfo.public_repos}</dd>
+            </dl>
+          `;
+          const result = document.getElementById("result");
+          result.innerHTML = view;
         });
       }
     })
     .catch(error => {
       console.error(error);
     });
+};
+
+const escapeHTML = (strings, ...values) => {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+};
+
+const escapeSpecialChars = str => {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 };
